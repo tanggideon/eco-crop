0b54fbd021a8fdd04f694b18e42fae600cc24eb1
tanggideon <tanggideon1@gmail.com>
2024-11-26 00:17:35 +0000

    made extra changes

--- ecocrop/src/app/(components)/orderChart/index.tsx
+++ ecocrop/src/app/(components)/orderChart/index.tsx
@@ -1,18 +1,123 @@
-"use client"
+"use client";
 
-import { Ellipsis } from 'lucide-react'
-import React from 'react'
+import { Ellipsis } from "lucide-react";
+import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "January",
+    retail: 12500,
+    wholesale: 25015,
+  },
+  {
+    name: "February",
+    retail: 12500,
+    wholesale: 45015,
+  },
+  {
+    name: "March",
+    retail: 5500,
+    wholesale: 5215,
+  },
+  {
+    name: "April",
+    retail: 2500,
+    wholesale: 215,
+  },
+  {
+    name: "May",
+    retail: 12500,
+    wholesale: 25015,
+  },
+  {
+    name: "June",
+    retail: 22500,
+    wholesale: 2515,
+  },
+  {
+    name: "July",
+    retail: 2500,
+    wholesale: 2015,
+  },
+  {
+    name: "August",
+    retail: 2500,
+    wholesale: 2015,
+  },
+  {
+    name: "September",
+    retail: 2500,
+    wholesale: 2015,
+  },
+  {
+    name: "October",
+    retail: 2500,
+    wholesale: 2015,
+  },
+  {
+    name: "November",
+    retail: 2500,
+    wholesale: 2015,
+  },
+  {
+    name: "December",
+    retail: 2500,
+    wholesale: 2015,
+  },
+];
 
 const OrderBarChart = () => {
   return (
-    <div>
-        <div className="flex items-center justify-between p-3">
-            <h1 className='text-xl font-semibold text-gray-700'>Daily Orders</h1>
-            <Ellipsis className="w-6 h-6 text-green-500 rounded-full cursor-pointer hover:bg-gray-200 hover:text-gray-500"/>
-        </div>
-        
+    <div className="bg-gray-100 py-5 h-full">
+      <div className="flex items-center justify-between p-3 h-[10%]">
+        <h1 className="text-xl font-semibold text-gray-700">Monthly Orders</h1>
+        <Ellipsis className="w-6 h-6 text-green-500 rounded-full cursor-pointer hover:bg-gray-200 hover:text-gray-500" />
+      </div>
+      <div className="w-full h-[90%]">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="wholesale"
+              stackId="1"
+              stroke="#8884d8"
+              fill="#33CC33B2"
+            />
+            <Area
+              type="monotone"
+              dataKey="retail"
+              stackId="1"
+              stroke="#82ca9d"
+              fill="#3380CCCC"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrderBarChart
+export default OrderBarChart;
--- ecocrop/src/app/(components)/revenue/index.tsx
+++ ecocrop/src/app/(components)/revenue/index.tsx
@@ -1,34 +1,92 @@
-"use client"
+"use client";
 
-import { Ellipsis } from 'lucide-react'
-import React from 'react'
+import { Ellipsis } from "lucide-react";
+import React from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Rectangle,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const data = [
+  {
+    name: "Sunday",
+    retail: 12500,
+    wholesale: 25015,
+  },
+  {
+    name: "Monday",
+    retail: 12500,
+    wholesale: 45015,
+  },
+  {
+    name: "Tuesday",
+    retail: 5500,
+    wholesale: 5215,
+  },
+  {
+    name: "Wednesday",
+    retail: 2500,
+    wholesale: 215,
+  },
+  {
+    name: "Thursday",
+    retail: 12500,
+    wholesale: 25015,
+  },
+  {
+    name: "Friday",
+    retail: 22500,
+    wholesale: 2515,
+  },
+  {
+    name: "Saturday",
+    retail: 2500,
+    wholesale: 2015,
+  },
+];
 
 const RevenueCharts = () => {
   return (
     <div className="bg-gray-100 py-5 h-full">
-        <div className="flex items-center justify-between p-3 h-[10%]">
-            <h1 className='text-xl font-semibold text-gray-700'>Revenue Generated</h1>
-            <Ellipsis className="w-6 h-6 text-green-500 rounded-full cursor-pointer hover:bg-gray-200 hover:text-gray-500"/>
-        </div>
-        <div className="w-full h-[80%]">
-          
-        </div>
-        <div className="flex items-center justify-around h-[10%]">
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <div className="w-5 h-5 rounded-full bg-green-300"/>
-            <h2 className="text-sm font-bold text-gray-500">Products</h2>
-          </div>
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <div className="w-5 h-5 rounded-full bg-blue-300"/>
-            <h2 className="text-sm font-bold text-gray-500">Suppliers</h2>
-          </div>
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <div className="w-5 h-5 rounded-full bg-yellow-300"/>
-            <h2 className="text-sm font-bold text-gray-500">Customers</h2>
-          </div>
-        </div>
+      <div className="flex items-center justify-between p-3 h-[10%]">
+        <h1 className="text-xl font-semibold text-gray-700">
+          Revenue Generated
+        </h1>
+        <Ellipsis className="w-6 h-6 text-green-500 rounded-full cursor-pointer hover:bg-gray-200 hover:text-gray-500" />
+      </div>
+      <div className="w-full h-[90%]">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart width={500} height={300} data={data} barSize={20}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend 
+            />
+            <Bar
+              dataKey="retail"
+              fill="#33CC33B2"
+              legendType="circle"
+              radius={[10, 10, 0, 0]}
+            />
+            <Bar
+              dataKey="wholesale"
+              fill="#3380CCCC"
+              legendType="circle"
+              radius={[10, 10, 0, 0]}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RevenueCharts
+export default RevenueCharts;
