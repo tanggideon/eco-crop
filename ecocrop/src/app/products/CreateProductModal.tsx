6b4409755b1023475c7350f90fa6c3bdae484dab
tanggideon <tanggideon1@gmail.com>
2024-11-26 01:07:36 +0000

    made extra changes

--- ecocrop/src/app/products/CreateProductModal.tsx
+++ ecocrop/src/app/products/CreateProductModal.tsx
@@ -1,101 +1,97 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import { v4 } from 'uuid';
-import Header from '../(components)/Header';
-import {zodResolver} from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import {
-  Form
-} from "@/components/ui/form"
+import React, { useState } from "react";
+import { v4 } from "uuid";
+import Header from "../(components)/Header";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
 
-import { Button } from '@/components/ui/button';
-import CustomFormField from '../(components)/customformfield';
-import { UserFormValidation } from '@/lib/validation';
-import SubmitButton from '../(components)/submitbutton';
+import { Button } from "@/components/ui/button";
+import CustomFormField from "../(components)/customformfield";
+import { UserFormValidation } from "@/lib/validation";
+import SubmitButton from "../(components)/submitbutton";
 
 export enum FormFieldType {
-    INPUT = "input",
-    TEXTAREA = "textarea",
-    PHONE_INPUT = "phoneInput",
-    CHECKBOX = "checkbox",
-    DATE_PICKER = "datePicker",
-    SELECT = "select",
-    SKELETON = "skeleton"
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
 }
-
-
-
 
 type ProductFormData = {
-    name: string;
-    description: string;
-    measurement: string;
-}
+  name: string;
+  description: string;
+  measurement: string;
+};
 
 type CreateProductProps = {
-    isOpen: boolean;
-    onClose: () => void;
-    // onCreate: (formData: ProductFormData) => void; 
-}
+  isOpen: boolean;
+  onClose: () => void;
+  // onCreate: (formData: ProductFormData) => void;
+};
 
-const CreateProductModal = ({isOpen, onClose}: CreateProductProps) => {
-    if (!isOpen) return null;
+const CreateProductModal = ({ isOpen, onClose }: CreateProductProps) => {
+  if (!isOpen) return null;
 
-    const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-    const form = useForm<z.infer<typeof UserFormValidation>>({
-        resolver: zodResolver(UserFormValidation),
-        defaultValues: {
-          name: "",
-          email: "",
-          phone: "",
-        },
-      })
-    
-    function onSubmit(values: z.infer<typeof UserFormValidation>) {
-      setIsLoading(true);
-      try {
-        console.log(values)
-      } catch (error) {
-        console.log(error);
-      }
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      console.log(values);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return (
-        
-
-    <div className='fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20 p-20' >
-        <div className='relative top-20 mx-auto border w-96 xl:w-[1024px] shadow-lg rounded-md bg-white p-10' >
-            <Header name="Create New Product" />
-            <hr/>
-            <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <CustomFormField 
-            inputType={FormFieldType.INPUT}
-            control={form.control} 
-            name="name"
-            label="Product Name"
-            placeholder="Product Name"
-            iconSrc=""
-            iconAlt="product icon"
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20 p-20">
+      <div className="relative top-20 mx-auto border w-96 xl:w-[1024px] shadow-lg rounded-md bg-white p-10">
+        <Header name="Create New Product" />
+        <hr />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6 flex-1"
+          >
+            <CustomFormField
+              inputType={FormFieldType.INPUT}
+              control={form.control}
+              name="name"
+              label="Product Name"
+              placeholder="Product Name"
+              iconSrc=""
+              iconAlt="product icon"
             />
-        <CustomFormField 
-            inputType={FormFieldType.PHONE_INPUT}
-            control={form.control} 
-            name="email"
-            label="Product Name"
-            placeholder="Product Name"
-            iconSrc=""
-            iconAlt="product icon"
+            <CustomFormField
+              inputType={FormFieldType.PHONE_INPUT}
+              control={form.control}
+              name="email"
+              label="Product Name"
+              placeholder="Product Name"
+              iconSrc=""
+              iconAlt="product icon"
             />
-        <SubmitButton isLoading={isLoading}>Add Product</SubmitButton>
-      </form>
-    </Form>
-        </div>
+            <SubmitButton isLoading={isLoading}>Add Product</SubmitButton>
+          </form>
+        </Form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateProductModal
+export default CreateProductModal;
--- ecocrop/src/app/suppliers/ViewDetails.tsx
+++ ecocrop/src/app/suppliers/ViewDetails.tsx
@@ -1,24 +1,55 @@
-import React from 'react'
-import Header from '../(components)/Header';
-
+import React from "react";
+import Header from "../(components)/Header";
+import Image from "next/image"
 
 type ViewDetailsProps = {
-    isOpen: boolean;
-    onClose: () => void;
-}
+  isOpen: boolean;
+  onClose: () => void;
+};
 
-const ViewDetailsModal = ({isOpen, onClose}: ViewDetailsProps) => {
-    if (!isOpen) return null;
-
+const ViewDetailsModal = ({ isOpen, onClose }: ViewDetailsProps) => {
+  if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20 p-20">
-        <div className='relative top-20 mx-auto border w-96 xl:w-[1024px]  shadow-lg rounded-md bg-white p-10' >
-            <Header name="Supplier Details"/>
+      <div className="relative top-20 mx-auto border w-96 xl:w-[1024px]  shadow-lg rounded-md bg-white p-10">
+        <Header name="Supplier Details" />
+        <hr />
+        <div className="w-full h-full py-5 px-10">
+          <div className="flex gap-10 m-5 w-full">
+            <div className="">
+            <Image src="/assets/path-cornfield-countryside.jpg" alt="crop photo" width={300} height={300}/>
+            </div>
+            <div className="flex flex-col gap-1 w-full">
+              <div className="flex flex-col items-start justify-start">
+                <span className="text-md font-semibold text-gray-400">Supplier Name:</span>
+                <h1 className="text-2xl font-semibold text-gray-700">Asuru Grace</h1>
+              </div>
+              <hr />
+              <div className="flex flex-col items-start justify-start">
+                <span className="text-md font-semibold text-gray-400">Email:</span>
+                <h1 className="text-xl font-semibold text-gray-700">Asurugrace000@gmail.com</h1>
+              </div>
+              <div className="flex flex-col items-start justify-start">
+                <span className="text-md font-semibold text-gray-400">Telephone:</span>
+                <h1 className="text-xl font-semibold text-gray-700">+233 201234567 / +233 591234567</h1>
+              </div>
+              <hr />
+              {/* <div className="flex flex-col items-start justify-start">
+                <span className="text-md font-semibold text-gray-400">Date Joined:</span>
+                <h1 className="text-l font-semibold text-gray-700">November 30th, 2024</h1>
+              </div>
+              <div className="flex flex-col items-start justify-start">
+                <span className="text-md font-semibold text-gray-400">Number of Listed Products:</span>
+                <h1 className="text-l font-semibold text-gray-700">7</h1>
+              </div> */}
+            </div>
+          </div>
+          
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ViewDetailsModal
+export default ViewDetailsModal;
